7a842969cedc9c6c83dde5c4b693be171cc2f513
Carlos Schmidt <18703981+carlos-schmidt@users.noreply.github.com>
2023-07-20 10:47:46 +0200

    Add ideas to resolve mirobot issue

--- miro.py
+++ miro.py
@@ -1,15 +1,25 @@
 from model import Config, Mirobot
 from opcua import Client
 
-# Change where to find config here. Also set section if needed
-config = Config(file='./config.cfg', section='DEFAULT') # section='BLE'
+# Change where to find config here
+_config_location: str = './config.cfg'
+# Also set section if needed
+_section: str = 'DEFAULT'  # Bluetooth Low Energy: 'BLE'
 
-opcua_client = Client(config.get_opcua_server_url(), config.get_opcua_request_timeout())
+config = Config(file=_config_location, section=_section)
+
+opcua_client = Client(config.get_opcua_server_url(),
+                      config.get_opcua_request_timeout())
 
 opcua_client.connect()
 
 robot = Mirobot(config)
 
 robot.initialize()
+
+# try unlock_shaft()?
+
+# put following line into mirobot/serial_interface.py:181
+# print(f"EOL threshold: {eol_threshold}")
 
 robot.disconnect()
