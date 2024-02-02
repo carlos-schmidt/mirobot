0d173e5e106bc7ae755fe9c9ab64303313f85926
Carlos Schmidt <18703981+carlos-schmidt@users.noreply.github.com>
2024-02-02 10:51:46 +0100

    Rename topic->node, make eventhandler own class

---
+++ model/event_listener.py
@@ -0,0 +1,63 @@
+import asyncio
+import logging
+from asyncua import Client, Node
+
+from model.config import Config
+
+logging.basicConfig(level=logging.INFO)
+el_logger = logging.getLogger("EventListener")
+
+
+class EventListener:
+    def __init__(self, config: Config):
+        self.config: Config = config
+        self.event_call_mutex = asyncio.Lock()
+
+    def mutex(func):
+        def mutexed_call(self):
+            with self.event_call_mutex:
+                func(self)
+
+        return mutexed_call
+
+    def datachange_notification(self, _, __, ___):
+        el_logger("This should be implemented by a EventListener subclass")
+
+    async def create_opcua_subscriptions(self):
+        utvr = zip(
+            self.config.opcua_server_urls,
+            self.config.opcua_nodes,
+            self.config.opcua_values,
+            self.config.opcua_routines,
+        )
+        clients = []
+
+        for url in set(self.config.opcua_server_urls):
+            new_client = Client(url=url)
+            clients.append(new_client)
+            await new_client.connect()
+
+            # Get nodes by nodeID
+            self.nodes_triggers_routines = [
+                (new_client.get_node(node_id), value, routine)
+                for _url, node_id, value, routine in utvr
+                if url == _url
+            ]
+
+            # One handler for all nodes. In datachange_notification the decision is made what to do.
+            subscription = await new_client.create_subscription(
+                self.config.opcua_polling_rate, self
+            )
+            # We subscribe to data changes for two nodes (variables).
+            await subscription.subscribe_data_change(self.config.opcua_nodes)
+
+        return clients
+
+    async def listen_for_opcua_events(self):
+        clients = await self.create_opcua_subscriptions()
+        try:
+            print("Subscribed to OPCUA nodes.")
+            await asyncio.sleep(999_999_999)
+        except KeyboardInterrupt:
+            el_logger.info("Disconnecting from opcua servers...")
+            [client.disconnect() for client in clients]
--- run.py
+++ run.py
@@ -1,14 +1,9 @@
 import asyncio
-from concurrent.futures import ProcessPoolExecutor
 import logging
-import os
-import sys
 import numpy as np
-from time import sleep
-from asyncua import Client, Node
-from mirobot import BaseMirobot, Mirobot
-from wlkata_mirobot import WlkataMirobot, WlkataMirobotTool
+
 from model.config import Config
+from model.event_listener import EventListener
 from model.mirobot_wrapper import Mirobot
 from model.robot_pose import RobotPose
 
@@ -16,11 +11,13 @@ logging.basicConfig(level=logging.WARN)
 mr_logger = logging.getLogger("MirobotRunner")
 
 
-class MirobotRunner:
-    def __init__(self, config: Config) -> None:
-        # self.robot = Mirobot(config.mirobot_portname, config.mirobot_debug)
-        # Nodes to listen for (there is a mapping topic->node)
-        self.triggers_and_routines = []
+class MirobotRunner(EventListener):
+    def __init__(self, config: Config):
+        super().__init__(config)
+
+        self.robot = Mirobot(config.mirobot_portname, config.mirobot_debug)
+
+        self.nodes_triggers_routines = []
         self.conveyor_belt_output_location = RobotPose(
             np.asarray(config.conveyor_belt_output_location)
         )
@@ -36,20 +33,19 @@ class MirobotRunner:
         ]
         self.stored_items: int = config.stored_items_initial
         self.zero_position = RobotPose(np.asarray((0.0, 0.0, 0.0, 0.0, 0.0, 0.0)))
-        self.config = config
 
-    def datachange_notification(self, node: Node, val, data):
+    def datachange_notification(self, node, val, data):
         """
         Callback for asyncua Subscription.
         This method will be called when the Client received a data change message from the Server.
         """
 
-        for trigger_node, trigger_value, routine in self.triggers_and_routines:
+        for trigger_node, trigger_value, routine in self.nodes_triggers_routines:
             if node == trigger_node and val == trigger_value:
                 mr_logger.warn("Starting robot interaction")
-                # Call routine by its name (possibly unsafe?)
+                # Call routine by its name
                 if hasattr(self, routine):
-                    routine: function = getattr(self, routine)
+                    routine = getattr(self, routine)
                     routine()
                 else:
                     mr_logger.error(
@@ -57,19 +53,23 @@ class MirobotRunner:
                     )
                 return
 
+    @EventListener.mutex
     def put_from_conveyor_belt_output(self):
-        mr_logger.info(f"ITEM-OUTPUT->ITEM-INPUT")
-        self.robot.move_along_trajectory(
-            self.conveyor_belt_output_location,
-            self.conveyor_belt_intermediate_locations,
-        )
-        self.robot.pick_up()
-        self.robot.move_along_trajectory(
-            self.conveyor_belt_input_location, self.conveyor_belt_intermediate_locations
-        )
-        self.robot.drop()
-        self.robot.go_to_zero()
+        with self.robo_mutex:
+            mr_logger.info(f"ITEM-OUTPUT->ITEM-INPUT")
+            self.robot.move_along_trajectory(
+                self.conveyor_belt_output_location,
+                self.conveyor_belt_intermediate_locations,
+            )
+            self.robot.pick_up()
+            self.robot.move_along_trajectory(
+                self.conveyor_belt_input_location,
+                self.conveyor_belt_intermediate_locations,
+            )
+            self.robot.drop()
+            self.robot.go_to_zero()
 
+    @EventListener.mutex
     def store_item(self):
         mr_logger.info(f"ITEM-OUTPUT->STORE[{self.stored_items}]")
         self.robot.move_along_trajectory(
@@ -85,6 +85,7 @@ class MirobotRunner:
 
         self.stored_items += 1
 
+    @EventListener.mutex
     def put_from_store(self):
         if self.stored_items < 1:
             mr_logger.warn("No items in store or wrong configuration values")
@@ -101,43 +102,6 @@ class MirobotRunner:
         self.robot.go_to_zero()
 
         self.stored_items -= 1
-
-    async def listen_for_opcua_events(self):
-        utvr = zip(
-            config.opcua_server_urls,
-            config.opcua_topics,
-            config.opcua_values,
-            config.opcua_routines,
-        )
-        clients = []
-
-        for url in set(self.config.opcua_server_urls):
-            new_client = Client(url=url)
-            clients.append(new_client)
-            await new_client.connect()
-
-            # Get nodes for topics
-            self.triggers_and_routines = [
-                (new_client.get_node(topic), value, routine)
-                for _url, topic, value, routine in utvr
-                if url == _url
-            ]
-
-            # One handler for all topics. In datachange_notification the decision is made what to do.
-            subscription = await new_client.create_subscription(
-                config.opcua_polling_rate, self
-            )
-            # We subscribe to data changes for two nodes (variables).
-            await subscription.subscribe_data_change(config.opcua_topics)
-
-        try:
-            print(
-                "Connected/subscribed to OPCUA servers/topics. Now waiting until keyboard interrupt"
-            )
-            await asyncio.sleep(999_999_999)
-        except KeyboardInterrupt:
-            mr_logger.info("Disconnecting from opcua servers...")
-            [client.disconnect() for client in clients]
 
 
 if __name__ == "__main__":
