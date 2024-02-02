b9eed89ce2fa8ab23ad228f303f99b9e7370eefb
Carlos Schmidt <18703981+carlos-schmidt@users.noreply.github.com>
2024-02-02 15:43:31 +0100

    Clean up opcua test server

--- model/event_listener.py
+++ model/event_listener.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-from asyncua import Client, Node
+from asyncua import Client
 
 from model.config import Config
 
@@ -33,23 +33,27 @@ class EventListener:
         clients = []
 
         for url in set(self.config.opcua_server_urls):
-            new_client = Client(url=url)
-            clients.append(new_client)
-            await new_client.connect()
+            print("Connecting to url", url)
+            async with Client(url=url) as new_client:
+                print("Connected to url", url)
 
-            # Get nodes by nodeID
-            self.nodes_triggers_routines = [
-                (new_client.get_node(node_id), value, routine)
-                for _url, node_id, value, routine in utvr
-                if url == _url
-            ]
+                clients.append(new_client)
 
-            # One handler for all nodes. In datachange_notification the decision is made what to do.
-            subscription = await new_client.create_subscription(
-                self.config.opcua_polling_rate, self
-            )
-            # We subscribe to data changes for two nodes (variables).
-            await subscription.subscribe_data_change(self.config.opcua_nodes)
+                await new_client.connect()
+
+                # Get nodes by nodeID
+                self.nodes_triggers_routines = [
+                    (new_client.get_node(node_id), value, routine)
+                    for _url, node_id, value, routine in utvr
+                    if url == _url
+                ]
+
+                # One handler for all nodes. In datachange_notification the decision is made what to do.
+                subscription = await new_client.create_subscription(
+                    self.config.opcua_polling_rate, self
+                )
+                # We subscribe to data changes for two nodes (variables).
+                await subscription.subscribe_data_change(self.config.opcua_nodes)
 
         return clients
 
--- test/opcua_server.py
+++ test/opcua_server.py
@@ -1,4 +1,5 @@
-from opcua import ua, uamethod, Server
+import asyncio
+from asyncua import ua, Server, Node
 from threading import Thread
 import logging
 import time
@@ -6,47 +7,6 @@ from math import sin
 import sys
 
 sys.path.insert(0, "..")
-
-try:
-    from IPython import embed
-except ImportError:
-    import code
-
-    def embed():
-        myvars = globals()
-        myvars.update(locals())
-        shell = code.InteractiveConsole(myvars)
-        shell.interact()
-
-
-class SubHandler(object):
-    """
-    Subscription Handler. To receive events from server for a subscription
-    """
-
-    def datachange_notification(self, node, val, data):
-        print("Python: New data change event", node, val)
-
-    def event_notification(self, event):
-        print("Python: New event", event)
-
-
-# method to be exposed through server
-
-def func(parent, variant):
-    ret = False
-    if variant.Value % 2 == 0:
-        ret = True
-    return [ua.Variant(ret, ua.VariantType.Boolean)]
-
-
-# method to be exposed through server
-# uses a decorator to automatically convert to and from variants
-
-@uamethod
-def multiply(parent, x, y):
-    print("multiply method call with parameters: ", x, y)
-    return x * y
 
 
 class VarUpdater(Thread):
@@ -59,60 +19,51 @@ class VarUpdater(Thread):
         self._stopev = True
 
     def run(self):
+        k = 0
         while not self._stopev:
-            v = int(sin(time.time() / 10)*5)
+            k += 1
+
+            v = 22 + k % 2
             self.var.set_value(v)
-            time.sleep(0.1)
+            time.sleep(2)
 
 
-if __name__ == "__main__":
+async def main():
     # optional: setup logging
     logging.basicConfig(level=logging.WARN)
 
     # now setup our server
     server = Server()
-    # server.disable_clock()
-    # server.set_endpoint("opc.tcp://localhost:4840/freeopcua/server/")
-    server.set_endpoint("opc.tcp://0.0.0.0:4840/freeopcua/server/")
+    await server.init()
+
+    server.set_endpoint("opc.tcp://localhost:4840/freeopcua/server/")
     server.set_server_name("FreeOpcUa Example Server")
+    
     # set all possible endpoint policies for clients to connect through
-    server.set_security_policy([
-        ua.SecurityPolicyType.NoSecurity,
-        ua.SecurityPolicyType.Basic256Sha256_SignAndEncrypt,
-        ua.SecurityPolicyType.Basic256Sha256_Sign])
+    server.set_security_policy([ua.SecurityPolicyType.NoSecurity])
 
     # setup our own namespace
     uri = "http://iosb.fraunhofer.example"
-    idx = server.register_namespace(uri)
+    idx = await server.register_namespace(uri)
 
     # populating our address space
 
     # create directly some objects and variables
-    myobj = server.nodes.objects.add_object(idx, "MeasuringStationStep")
-    myvar = myobj.add_variable(idx, "MyVariable", 22, ua.VariantType.Int16)
-
-    # creating a default event object
-    # The event object automatically will have members for all events properties
-    # you probably want to create a custom event type, see other examples
-    myevgen = server.get_event_generator()
-    myevgen.event.Severity = 300
+    myobj: Node = await server.nodes.objects.add_object(idx, "MeasuringStationStep")
+    myvar = await myobj.add_variable(idx, "MyVariable", 22, ua.VariantType.Int16)
 
     # starting!
-    server.start()
-
-    vup = VarUpdater(myvar)  # just  a stupid class update a variable
-    vup.start()
+    await server.start()
+    # print(await server.get_endpoints())
+    # vup = VarUpdater(myvar)  # just a class to update the variable
+    # vup.start()
     try:
-        # enable following if you want to subscribe to nodes on server side
-        # handler = SubHandler()
-        # sub = server.create_subscription(500, handler)
-        # handle = sub.subscribe_data_change(myvar)
-
-        myevgen.trigger(message="This is BaseEvent")
-        # Server side write method which is a bit faster than using set_value
-        server.set_attribute_value(myvar.nodeid, ua.DataValue(9))
-
-        embed()
-    finally:
+        while True:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
         vup.stop()
-        server.stop()
+        await server.stop()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
