96b2dc76f085eb0e319b6e03d0b3426599d0ac82
Carlos Schmidt <18703981+carlos-schmidt@users.noreply.github.com>
2024-03-19 14:36:42 +0100

    Move manual_go_to to test directory

--- src/test/manual_go_to.py
+++ src/test/manual_go_to.py
@@ -1,17 +1,19 @@
 import numpy as np
-from wlkata_mirobot import WlkataMirobot, WlkataMirobotTool
+from wlkata_mirobot import WlkataMirobot
 
 from src.model.config import Config
 from src.model.robot_pose import RobotPose
 
+
+# Playground where you can set custom positions for the robot go move to
 config = Config("./custom-config.cfg", "DEFAULT")
 
-bot = WlkataMirobot(portname='COM8', default_speed=9999)
+bot = WlkataMirobot(portname='COM8', default_speed=3000)
 store_locations = [RobotPose(np.asarray(pos)) for pos in config.store_locations]
 
+# Choose intermediate position
 bot.linear_interpolation(134.034,-93.823,188.632,0.000,13.001,-34.992)
-for store_location in store_locations[5:]:
-    break
-    store_intermediate = store_location - RobotPose(np.asarray([0,0,-50,0,0,0])) # Z coord + 50
+for store_location in store_locations:
+    store_intermediate = store_location + RobotPose(np.asarray([0,0,50,0,0,0])) # Z coord + 50
     #bot.linear_interpolation(*store_intermediate.astuple())
     bot.linear_interpolation(*store_location.astuple())
